ae8b2be53eafe1fd4568c3ea1b42f521221b0d3f
Leo Liu <zmliu0077@gmail.com>
2019-08-05 09:16:00 +0800

    首页轮播图

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/service/service_ method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据';
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('商城首页'),
+        appBar: AppBar(
+          title: Text('百姓生活+'),
+        ),
+        body: FutureBuilder(
+          future: getHomePageContent(),
+          builder: (context, snapshop) {
+            if (snapshop.hasData) {
+              var data = json.decode(snapshop.data.toString());
+              List<Map> swiper = (data['data']['slides'] as List).cast();
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(
+                    swiperDataList: swiper,
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('加载中.....'),
+              );
+            }
+          },
+        ));
+  }
+}
+
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+
+  SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 设计稿的尺寸
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    print('设备宽度:${ScreenUtil.screenWidth}');
+    print('设备高度:${ScreenUtil.screenHeight}');
+    print('设备像素密度:${ScreenUtil.pixelRatio}');
+    return Container(
+      height: ScreenUtil().setHeight(333),
+//      width: ScreenUtil().setHeight(750),
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "${swiperDataList[index]['image']}",
+            fit: BoxFit.fitWidth,
+          );
+        },
+        itemCount: swiperDataList.length,
+        // 显示底下的点
+        pagination: new SwiperPagination(),
+        autoplay: true,
       ),
     );
   }
