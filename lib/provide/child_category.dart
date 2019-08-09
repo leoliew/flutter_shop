8baeb47ea03fc8e0015ead46370c642b7666ee6d
Leo Liu <zmliu0077@gmail.com>
2019-08-10 07:12:09 +0800

    点击切换改变颜色

--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -4,8 +4,11 @@ import 'package:flutter_shop/model/category.dart';
 //ChangeNotifier的混入是不用管理听众
 class ChildCategory with ChangeNotifier {
   List<BxMallSubDto> childCategoryList = [];
+  int childIndex = 0; // 子类高亮索引
 
+  // 大类切换逻辑
   getChildCategory(List<BxMallSubDto> list) {
+    childIndex = 0;
     BxMallSubDto all = BxMallSubDto();
     all.mallSubId = '00';
     all.mallCategoryId = '00';
@@ -13,6 +16,13 @@ class ChildCategory with ChangeNotifier {
     all.comments = 'null';
     childCategoryList = [all];
     childCategoryList.addAll(list);
+    notifyListeners();
+  }
+
+  // 改变子类索引
+
+  changeChildIndex(int index){
+    childIndex = index;
     notifyListeners();
   }
 }
