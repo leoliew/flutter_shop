1bcfb6da7bb493539d947d171354d42ec5aa0c6c
Leo Liu <zmliu0077@gmail.com>
2019-08-10 15:21:33 +0800

    商品详细页

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
 import 'package:flutter_shop/provide/details_info.dart';
+import 'package:flutter_shop/pages/details_page/details_top_area.dart';
 
 class DetailPage extends StatelessWidget {
   final String goodsId;
@@ -14,32 +15,30 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(
         leading: IconButton(
             icon: Icon(Icons.arrow_back),
-            onPressed: (){
+            onPressed: () {
               Navigator.pop(context);
-            }
-        ),
+            }),
         title: Text('商品详细页'),
       ),
       body: FutureBuilder(
-        future: _getBackInfo(context),
-          builder: (context,snapshot){
-          if(snapshot.hasData){
-            return Container(
-              child: Column(
-                children: <Widget>[
-                  Text('获取到数据${goodsId}')
-                ],
-              ),
-            );
-          }else {
-            return Text('加载中');
-          }
-          }
-      ),
+          future: _getBackInfo(context),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Container(
+                child: Column(
+                  children: <Widget>[
+                    DetailsTopArea(),
+                  ],
+                ),
+              );
+            } else {
+              return Text('加载中');
+            }
+          }),
     );
   }
 
-  Future _getBackInfo(BuildContext context)async {
+  Future _getBackInfo(BuildContext context) async {
     await Provide.value<DetailsInfoProvide>(context).getGoodsInfo(goodsId);
     return '完成加载';
   }
