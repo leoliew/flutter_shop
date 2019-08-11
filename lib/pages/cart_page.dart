55098af86ce284e27a604f6e6edd4919ee7d968c
Leo Liu <zmliu0077@gmail.com>
2019-08-11 19:48:03 +0800

    购物车ui

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,47 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
-import 'package:flutter_shop/provide/counter.dart';
+import 'package:flutter_shop/provide/cart.dart';
+import 'package:flutter_shop/pages/cart_page/cart_item.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        children: <Widget>[
-          Number(),
-          MyButton(),
-        ],
+      appBar: AppBar(
+        title: Text('购物车'),
       ),
-    ));
-  }
-}
-
-class Number extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 200),
-      child: Provide<Counter>(builder: (context, child, counter) {
-        return Text(
-          '${counter.val}',
-          style: Theme.of(context).textTheme.display1,
-        );
-      }),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        onPressed: () {
-          Provide.value<Counter>(context).increment();
+      body: FutureBuilder(
+        future:_getCartInfo(context),
+        builder: (context,snapshot){
+          List cartList= Provide.value<CartProvide>(context).cartList;
+          if(snapshot.hasData){
+            print(cartList);
+            return ListView.builder(
+              itemCount: cartList.length,
+              itemBuilder: (context,index){
+                return CartItem(cartList[index]);
+              },
+            );
+          }else{
+            return Text('正在加载');
+          }
         },
-        child: Text('递增'),
       ),
     );
+
+  }
+
+
+  Future<String> _getCartInfo(BuildContext context) async{
+    await Provide.value<CartProvide>(context).getCartInfo();
+    return 'end';
   }
 }
--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_shop/model/cartInfo.dart';
 import 'dart:convert';
 
 class CartProvide with ChangeNotifier {
   String cartString = '[]';
+  List<CartInfoModel> cartList= [];
 
   save(goodsId, goodsName, count, price, images) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -15,23 +17,27 @@ class CartProvide with ChangeNotifier {
     tempList.forEach((item) {
       if (item['goodsId'] == goodsId) {
         tempList[ival]['count'] = item['count'] + 1;
+        cartList[ival].count++;
         isHave = true;
       }
       ival++;
     });
 
     if (!isHave) {
-      tempList.add({
-        'goodsId': goodsId,
-        'goodsName': goodsName,
-        'count': count,
-        'price': price,
-        'images': images
-      });
+      Map<String, dynamic> newGoods={
+        'goodsId':goodsId,
+        'goodsName':goodsName,
+        'count':count,
+        'price':price,
+        'images':images
+      };
+      tempList.add(newGoods);
+      cartList.add(new CartInfoModel.fromJson(newGoods));
     }
 
     cartString = json.encode(tempList).toString();
-    print(cartString);
+    print('字符串>>>>>>>>>>>${cartString}');
+    print('数据模型>>>>>>>>>>>${cartList}');
     prefs.setString('cartInfo', cartString);
     notifyListeners();
 
@@ -41,7 +47,24 @@ class CartProvide with ChangeNotifier {
   remove() async{
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('cartInfo');
+    cartList = [];
     print('清空完成.....');
     notifyListeners();
+  }
+
+  getCartInfo() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    cartList=[];
+    if(cartString==null){
+      cartList=[];
+    }else{
+      List<Map> tempList= (json.decode(cartString.toString()) as List).cast();
+      tempList.forEach((item){
+        cartList.add(new CartInfoModel.fromJson(item));
+      });
+    }
+    notifyListeners();
+
   }
 }
